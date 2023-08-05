18557b42f80814d3d6c8c891327c916354770898
Sheema-parveen <141474798+Sheema-parveen@users.noreply.github.com>
2023-08-05 13:11:26 +0530

    adding comment line in 1st line

--- basicprogram.py
+++ basicprogram.py
@@ -1,3 +1,4 @@
+#selenium automation with firefoxbrowser-GeckoDriverManager
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
